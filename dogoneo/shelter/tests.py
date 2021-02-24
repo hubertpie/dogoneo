ef1d6cdf141772a445cef0e86b6124111f1b850a
hubertpie <68980276+hubertpie@users.noreply.github.com>
2021-02-24 19:08:14 +0100

    first unit test

--- dogoneo/shelter/tests.py
+++ dogoneo/shelter/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
+from shelter.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-	def test_bad_math(self):
-		self.assertEqual(1+1, 3)
+	def test_root_url_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
