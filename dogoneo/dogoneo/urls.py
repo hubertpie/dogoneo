5ffa096cea49ace3a7f358650d1db1146db8d346
hubertpie <68980276+hubertpie@users.noreply.github.com>
2021-03-07 17:59:20 +0100

    added static to urls

--- dogoneo/dogoneo/urls.py
+++ dogoneo/dogoneo/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from shelter import views as shelter_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', shelter_views.dog_list)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
